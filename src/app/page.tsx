c7ca8f06dea2c3e8ff69da4b3e4a9eb2079c912e
asnaweevixvify <asnaweeezor@gmail.com>
2026-02-14 22:08:53 +0700

    force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Home from "./pages/home/Home";
 import { blogService } from "@/infra/container";
 
+export const dynamic = "force-dynamic";
+
 export default async function page() {
   const blogs = await blogService.getBlog();
   return <Home blogs={blogs} />;
